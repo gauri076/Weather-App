749664cb5a9a74e1772e315d54c369acf2690730
Gauri076 <rathorgauri281@gmail.com>
2025-11-27 19:11:07 +0530

    completed with the styling

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const button = document.getElementById("search-btn");
 const input = document.getElementById("city-input");
 
 const cityNames = document.getElementById("city-names");
+const cityNames1 = document.getElementById("city-names1");
+const cityNames2 = document.getElementById("city-names2");
 const cityTime = document.getElementById("city-time");
 const tempCelcius = document.getElementById("celcius");
 const tempFeranite = document.getElementById("feranite");
@@ -19,9 +21,11 @@ button.addEventListener("click", async () => {
     const value = input.value;
     const result = await getData(value);
     console.log(result);
-    cityNames.innerText =`${result.location.name}, ${result.location.region} - ${result.location.country}`;
-    cityTime.innerText =`${result.location.localtime}`;
-    tempCelcius.innerText = `${result.current.temp_c}`;
-    tempFeranite.innerText = `temprature in Feranite:${result.current.temp_f}`
+    cityNames.innerText =`Name: ${result.location.name}`;
+    cityNames1.innerText =`Region: ${result.location.region}`;
+    cityNames2.innerText =` Country: ${result.location.country}`;
+    cityTime.innerText =`Time: ${result.location.localtime}`;
+    tempCelcius.innerText = `temprature in Celcius: ${result.current.temp_c}`;
+    tempFeranite.innerText = `temprature in Feranite: ${result.current.temp_f}`
     
 });
