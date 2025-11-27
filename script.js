fcc87e5ab555259adc011f37f57ef8bc0f747f89
Gauri076 <rathorgauri281@gmail.com>
2025-11-27 17:32:01 +0530

    weather app in html and js

---
+++ script.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("search-btn");
+const input = document.getElementById("city-input");
+const cityNames = document.getElementById("city-names");
+const cityTime = document.getElementById("city-time");
+const tempCelcius = document.getElementById("celcius");
+const tempFeranite = document.getElementById("feranite");
+
+async function getData(cityName){
+    const promise = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=6e9d1d7c2d9e43b08e371309252711 &q=${cityName}&aqi=yes`
+    );
+
+    return await promise.json()
+}
+
+
+button.addEventListener("click", async () => {
+    const value = input.value;
+    const result = getData(value);
+    cityNames.innerText =`${result.location.name}, ${result.location.region} - ${result.location.country}`;
+    
+});
